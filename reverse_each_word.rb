33f04a6a9f682ba74c7004851243860a39a67c21
Khadijah Jennings <kjennings855@gmail.com>
2019-12-14 09:59:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 sentence1 = "Hello there, and how are you?"
 def reverse_each_word(sentence1)
   reversed_sentence = [] 
-    sentence1.each do |word|
-    reversed_sentence.reverse
+    sentence1.each do |word|.reverse
   end
 reverse_each_word
 end
